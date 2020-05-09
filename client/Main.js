bde5a20e353b6189f814c807ed3a8f2fa657b607
James O'Reilly <James.Oreilly926@gmail.com>
2020-05-08 23:23:14 -0400

    add ApolloProvider wrapper around the app

--- client/Main.js
+++ client/Main.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+
 import Dance from './components/Dance'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8332/graphql'
+})
 
 const Main = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path='/dance' component={Dance} />
-      </Switch>
+      <ApolloProvider client={client}>
+        <Switch>
+          <Route exact path='/dance' component={Dance} />
+        </Switch>
+      </ApolloProvider>
     </Router>
-    )
+  )
 }
 
 export default Main
--- client/index.js
+++ client/index.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 import Main from './Main'
 
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8332/graphql'
+})
+
 ReactDOM.render(
-        <Main />,
-    document.getElementById('app')
+  <ApolloProvider client={client}>
+    <Main />
+  </ApolloProvider>,
+  document.getElementById('app')
 )
