de8647cf889f94624c8d0b10a18e6bb9796f476a
James O'Reilly <James.Oreilly926@gmail.com>
2020-05-09 16:02:58 -0400

    add the Cart and Item models with their association to the models/index.js file

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,21 +2,28 @@ const Product = require('./product')
 const Category = require('./category')
 const ProductCategory = require('./productCategory')
 const Review = require('./review')
+const Cart = require('./cart')
+const Item = require('./item')
 
 /***** Associations ******/
 
-//Product to Category
+//Product and Category
 
 Category.belongsToMany(Product, { through: ProductCategory })
 Product.belongsToMany(Category, { through: ProductCategory })
 
-//Product to Review
+//Product and Review
 Product.hasMany(Review, { as: 'review' })
 
+//Cart and Item
+Cart.hasMany(Item, { as: 'item' })
+Item.belongsTo(Cart)
 
 module.exports = {
   Product,
   Category,
   ProductCategory,
-  Review
+  Review,
+  Cart,
+  Item
 }
