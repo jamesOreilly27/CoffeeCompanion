b4a4a72faca032a126946b12196817febb6d14c6
James O'Reilly <James.Oreilly926@gmail.com>
2020-05-09 13:19:54 -0400

    add reviews to the ProductType GraphQL object

--- server/graphql/schema/ObjectTypes.js
+++ server/graphql/schema/ObjectTypes.js
@@ -1,5 +1,44 @@
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLNonNull, GraphQLSchema, GraphQLList } = require('graphql')
 
+const ProductType = new GraphQLObjectType({
+  name: 'product',
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    price: { type: GraphQLNonNull(GraphQLInt) },
+    inventory: { type: GraphQLNonNull(GraphQLInt) },
+    image: { type: GraphQLString },
+    categories: {
+      type: new GraphQLList(CategoryType),
+      description: 'category products',
+      resolve: product => {
+        return product.getCategories()
+        .then(categories => categories)
+        .catch(err => console.log(err))
+      }
+    },
+    reviews: {
+      type: new GraphQLList(ReviewType),
+      description: 'a list of reviews for this product',
+      resolve: product => {
+        return product.getReview()
+        .then(reviews => reviews)
+        .catch(err => console.log(err))
+      }
+    }
+  })
+})
+
+const ReviewType = new GraphQLObjectType({
+  name: 'review',
+  description: 'a review of a specific product',
+  fields: () => ({
+    content: { type: GraphQLNonNull(GraphQLString) },
+    rating: { type: GraphQLNonNull(GraphQLInt) }
+  })
+})
+
 const CategoryType = new GraphQLObjectType({
   name: 'category',
   description: 'Different groupings of products',
@@ -18,28 +57,8 @@ const CategoryType = new GraphQLObjectType({
   })
 })
 
-const ProductType = new GraphQLObjectType({
-  name: 'product',
-  fields: () => ({
-    id: { type: GraphQLNonNull(GraphQLInt) },
-    name: { type: GraphQLString },
-    description: { type: GraphQLString },
-    price: { type: GraphQLNonNull(GraphQLInt) },
-    inventory: { type: GraphQLNonNull(GraphQLInt) },
-    image: { type: GraphQLString },
-    categories: {
-      type: new GraphQLList(CategoryType),
-      description: 'category products',
-      resolve: product => {
-        return product.getCategories()
-        .then(categories => categories)
-        .catch(err => console.log(err))
-      }
-    }
-  })
-})
-
 module.exports = {
   CategoryType,
-  ProductType
+  ProductType,
+  ReviewType
 }
