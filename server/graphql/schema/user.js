401167166eafb6a8c1dc05b4d933e836eabbbe61
James O'Reilly <James.Oreilly926@gmail.com>
2020-05-12 09:03:16 -0400

    remove old console.log

--- server/graphql/schema/user.js
+++ server/graphql/schema/user.js
@@ -1,7 +1,6 @@
 const { User } = require('../../db/models')
 
 const userResolver = (parent, args, request) => {
-  console.log('FIRING USER RESOLVER')
   return User.findOne({ where: { email: request.user.email } })
   .then(user => user)
   .catch(err => console.log(err))
