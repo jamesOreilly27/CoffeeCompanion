baa8517c9738b2e5f3697d32de3d4581da142134
James O'Reilly <James.Oreilly926@gmail.com>
2020-05-14 13:44:27 -0400

    set up the basic layout for the UserAccount component

---
+++ client/components/user-account/ActiveCart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ActiveCart = () => {
+  return (
+    <div>
+      Hello World
+    </div>
+  )
+}
+
+export default ActiveCart
--- client/components/user-account/UserAccount.js
+++ client/components/user-account/UserAccount.js
@@ -3,17 +3,50 @@ import styled from 'styled-components'
 
 const Wrapper = styled.div`
   display: flex;
+  justify-content: center;
   flex-wrap: wrap;
   width: 98vw;
 `
 
 const Container = styled.div`
-
+  @media(max-width: 960px) {
+    flex: 0 0 30%
+  }
+  @media(max-width: 640px) {
+    flex: 0 0 40%;
+  }
+  @media(max-width: 450px) {
+    flex: 0 0 85%;
+    margin: 8px 7.5%;
+  }
+  height: 100px;
+  border: 1px solid black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 8px;
 `
 
 const UserAccount = ({ user }) => (
   <Wrapper>
-
+    <Container>
+      Your Orders
+    </Container>
+    <Container>
+      Your Orders
+    </Container>
+    <Container>
+      Your Orders
+    </Container>
+    <Container>
+      Your Orders
+    </Container>
+    <Container>
+      Your Orders
+    </Container>
+    <Container>
+      Your Orders
+    </Container>
   </Wrapper>
 )
 
