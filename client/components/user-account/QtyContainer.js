463e9e76022f088f7377afd9692c25db54048240
James O'Reilly <James.Oreilly926@gmail.com>
2020-05-18 00:21:28 -0400

    create the Element styled component in QtyContainer

--- client/components/user-account/QtyContainer.js
+++ client/components/user-account/QtyContainer.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons'
 
 const Wrapper = styled.div`
   display: flex;
@@ -10,44 +8,34 @@ const Wrapper = styled.div`
   border: 1px solid #000;
 `
 
-const Quantity = styled.div`
-  font-size: 11px;
+const Element = styled.div`
   display: flex;
-  align-items: center;
   justify-content: center;
+  align-items: center;
   height: 100%;
   width: 33%;
 `
 
-const Minus = styled.div`
+const Quantity = styled(Element)`
+  font-size: 11px;
+`
+
+const Minus = styled(Element)`
   font-size: 16px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
   border-right: 1px solid #000;
-  width: 33%;
 `
 
 const Plus = styled.div`
   font-size: 12px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
   border-left: 1px solid #000;
-  width: 33%;
 `
 
 const QtyContainer = ({ quantity }) => {
   return (
     <Wrapper>
-      {console.log('QTY', quantity)}
-      {/* <FontAwesomeIcon icon={faMinus} size="xs" /> */}
-        <Minus> - </Minus>
-        <Quantity>{quantity}</Quantity>
-        <Plus> + </Plus>
-      {/* <FontAwesomeIcon icon={faPlus} size="xs" /> */}
+      <Minus> - </Minus>
+      <Quantity>{quantity}</Quantity>
+      <Plus> + </Plus>
     </Wrapper>
   )
 }
