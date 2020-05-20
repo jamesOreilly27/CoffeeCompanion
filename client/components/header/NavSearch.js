d29231df8261b2877d0a5c804934e0d68495ff9c
James O'Reilly <James.Oreilly926@gmail.com>
2020-05-20 18:06:14 -0400

    delete the Dropdown component

--- client/components/header/NavSearch.js
+++ client/components/header/NavSearch.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 import { graphql } from 'react-apollo'
 import { getAllCategories } from '../../graphql'
-import { Dropdown, FormFill, SubmitButton } from '../header'
+import { Autocomplete } from '@material-ui/lab'
+import { TextField } from '@material-ui/core'
+import { FormFill, SubmitButton } from '../header'
 
-const Wrapper = styled.form`
+const Wrapper = styled(Autocomplete)`
   display: flex;
   align-items: center;
+  width: 150px;
+  padding: 0;
+  * {
+    padding: 0;
+  }
 `
 
 class NavSearch extends Component {
@@ -16,8 +23,9 @@ class NavSearch extends Component {
 
   render() {
     return (
-    <Wrapper>
-      {this.props.data.categories && <Dropdown categories={this.props.data.categories} />}
+    <Wrapper
+      renderInput={() => <FormFill />}
+    >
       <FormFill />
       <SubmitButton />
     </Wrapper>
