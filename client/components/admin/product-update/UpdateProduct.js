70f5e336cb5113edeead2866f40da33adbd83829
James O'Reilly <James.Oreilly926@gmail.com>
2020-05-30 00:33:03 -0400

    write Product details for the UpdateProduct component

--- client/components/admin/product-update/UpdateProduct.js
+++ client/components/admin/product-update/UpdateProduct.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import { Title, Image, InfoContainer, PriceContainer, PriceHeader, Price } from '../../styled-components'
+import { ReviewDisplay } from '../../product-detail'
 import { getProductDetail } from '../../../graphql'
 import { graphql } from 'react-apollo'
 import { urlToName } from '../../helpers'
 
 const Wrapper = styled.div`
-
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `
 
 const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`
 
+const DetailContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `
 
 class UpdateProduct extends Component {
@@ -23,7 +35,17 @@ class UpdateProduct extends Component {
       <Wrapper>
         {product &&
           <Container>
-            Hello From UpdateProduct
+            <DetailContainer>
+              <Image icon={['fa', 'image']} size="10x"></Image>
+              <InfoContainer>
+                <Title>{product.name}</Title>
+                <ReviewDisplay reviews={product.reviews} />
+                <PriceContainer>
+                  <PriceHeader>Our Price</PriceHeader>
+                  <Price>{`$${product.price}`}</Price>
+                </PriceContainer>
+              </InfoContainer>
+            </DetailContainer>
           </Container>
         }
       </Wrapper>
