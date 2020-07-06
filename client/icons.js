8391b2f6290d27791f66e9b5a9608ac6b0360e35
James O'Reilly <James.Oreilly926@gmail.com>
2020-07-06 17:20:18 -0400

    remove old icons from icons,js

--- client/icons.js
+++ client/icons.js
@@ -1,20 +1,10 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faImage } from '@fortawesome/free-solid-svg-icons/faImage'
 import { faSearch } from '@fortawesome/free-solid-svg-icons/faSearch'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons/faShoppingCart'
 import { faFacebook } from '@fortawesome/free-brands-svg-icons/faFacebook'
 import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter'
 import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin'
-import { faCar } from '@fortawesome/free-solid-svg-icons/faCar'
-import { faAnchor } from '@fortawesome/free-solid-svg-icons/faAnchor'
-import { faTractor } from '@fortawesome/free-solid-svg-icons/faTractor'
-import { faCannabis } from '@fortawesome/free-solid-svg-icons/faCannabis'
-import { faDumpster } from '@fortawesome/free-solid-svg-icons/faDumpster'
-import { faTools } from '@fortawesome/free-solid-svg-icons/faTools'
-import { faWarehouse } from '@fortawesome/free-solid-svg-icons/faWarehouse'
-import { faTruck } from '@fortawesome/free-solid-svg-icons/faTruck'
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons/faUserCircle'
 
 
 library.add(
