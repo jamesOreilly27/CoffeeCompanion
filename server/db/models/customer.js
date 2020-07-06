ef2bdd452c430592114ff7cdba8beebf96479b76
James O'Reilly <James.Oreilly926@gmail.com>
2020-07-06 16:30:58 -0400

    define the customer model

---
+++ server/db/models/customer.js
@@ -0,0 +1,33 @@
+const db = require('../db')
+const Sequelize = require('sequelize')
+
+const Customer = db.define('customer', {
+  companyName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  industry: {
+    type: Sequelize.STRING,
+  },
+  address: {
+    type: Sequelize.STRING
+  },
+  town: {
+    type: Sequelize.STRING
+  },
+  zipCode: {
+    type: Sequelize.STRING
+  },
+  phoneNumber: {
+    type: Sequelize.STRING
+  },
+  localPoliceNumber: {
+    type: Sequelize.STRING
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+})
+
+module.exports = Customer
